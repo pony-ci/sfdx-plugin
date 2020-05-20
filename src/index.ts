d088799e81b3a61ae3d578afef1ae48488793fc3
Ondřej Kratochvíl <ondrakratochvil96@gmail.com>
2020-05-20 15:32:44 +0200

    Fix schema2ts.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+
+export * from './lib/app';
+export * from './lib/PonyOrg';
+export * from './lib/PonyProject';
+export * from './lib/sfdx';
+export * from './lib/metadata/components';
+export * from './lib/metadata/describe-metadata';
+export * from './lib/ux';
+
+export * from './type-guards/schema';
+
+export * from './types/config.schema';
+export * from './types/package-group.schema';
