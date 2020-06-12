b4d4cdd8cb57671d81e0565252d73f5150ca4eef
Ondřej Kratochvíl <ondrakratochvil96@gmail.com>
2020-06-12 15:03:34 +0200

    fix

--- src/lib/metadata/sortDefinitions.ts
+++ src/lib/metadata/sortDefinitions.ts
@@ -1,12 +1,12 @@
-import {Dictionary, isJsonMap, isString} from '@salesforce/ts-types';
+import {Dictionary, isAnyJson, isJsonMap, isString} from '@salesforce/ts-types';
 import {MetadataType} from './describeMetadata';
 
 const innerTextSortKey = '__inner_text__';
 
-export const isSortedByInnerText = (it: any) =>
-    isJsonMap(it) && Object.keys(it).length === 1 && isInnerTextSortKey(Object.values(it)[0]);
+export const isSortedByInnerText = (it: unknown) =>
+    isAnyJson(it) && isJsonMap(it) && Object.keys(it).length === 1 && isInnerTextSortKey(Object.values(it)[0]);
 
-export const isInnerTextSortKey = (it: any) => isString(it) && it === innerTextSortKey;
+export const isInnerTextSortKey = (it: unknown) => isString(it) && it === innerTextSortKey;
 
 export type SortDefinition = Dictionary<'__inner_text__' | string[]>[];
 
