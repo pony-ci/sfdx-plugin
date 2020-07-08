215f0ccfad8e8532ca972374fdc91bfc24fafb8e
Ondřej Kratochvíl <ondrakratochvil96@gmail.com>
2020-07-08 14:26:28 +0200

    update user create command

--- src/commands/pony/user/create.ts
+++ src/commands/pony/user/create.ts
@@ -1,4 +1,5 @@
 import {flags, FlagsConfig} from '@salesforce/command';
+import {VarargsConfig} from '@salesforce/command/lib/sfdxCommand';
 import {sfdx} from '../../..';
 import PonyCommand from '../../../lib/PonyCommand';
 import PonyProject from '../../../lib/PonyProject';
@@ -12,45 +13,46 @@ export default class UserCreateCommand extends PonyCommand {
     public static readonly requiresProject: boolean = true;
 
     public static readonly flagsConfig: FlagsConfig = {
-        name: flags.string({
-            char: 'n',
-            description: 'comma-separated list of user definition names',
-            required: true
+        setalias: flags.string({
+            char: 'a',
+            description: 'alias for the created org',
+            required: false,
+        }),
+        prefix: flags.string({
+            char: 'p',
+            description: 'username prefix',
+            required: false
+        }),
+        definitionfile: flags.string({
+            char: 'f',
+            description: 'file path to a user definition',
+            required: false,
         })
     };
 
+    protected static varargs: VarargsConfig = {required: false};
+    protected static strict: boolean = false;
+
     public async run(): Promise<void> {
+        const varargs = this.varargs || {};
         const project = await PonyProject.load();
-        const {users = {}} = project.ponyConfig;
         const d = new Date();
-        const names = this.flags.name.split(',').map(it => it.trim());
-        for (const name of names) {
-            if (!(name in users)) {
-                throw Error(`No user definition for ${name}`);
-            }
-            const {
-                setAlias: setalias,
-                definitionFile: definitionfile,
-                usernamePrefix,
-                Username
-            } = users[name];
-            const args = Object.entries(users[name]).filter(([key]) => ![
-                'definitionFile',
-                'usernamePrefix'
-            ].includes(key)).map(([key, value]) => `${key}=${value}`);
-            if (!Username) {
-                const prefix = usernamePrefix ? `${usernamePrefix}-` : '';
-                args.push(`Username=${prefix}${project.getNormalizedProjectName()}-${d.valueOf()}@pony.user`);
-            }
-            const targetusername = this.org?.getUsername();
-            const targetdevhub = await this.org?.getDevHubOrg();
-            const targetdevhubusername = targetdevhub?.getUsername();
-            await sfdx.force.user.create({
-                targetusername,
-                targetdevhubusername,
-                setalias,
-                definitionfile,
-            }, args);
+        const {prefix, definitionfile, setalias} = this.flags;
+        const args = Object.entries(varargs)
+            .map(([key, value]) => `${key}=${value}`);
+        if (!('Username' in varargs)) {
+            const usernamePrefix = prefix ? `${prefix}-` : '';
+            args.push(`Username=${usernamePrefix}${project.getNormalizedProjectName()}-${d.valueOf()}@pony.user`);
         }
+        const targetusername = this.org?.getUsername();
+        const targetdevhub = await this.org?.getDevHubOrg();
+        const targetdevhubusername = targetdevhub?.getUsername();
+        console.log(args);
+        await sfdx.force.user.create({
+            targetusername,
+            targetdevhubusername,
+            setalias,
+            definitionfile,
+        }, args);
     }
 }
